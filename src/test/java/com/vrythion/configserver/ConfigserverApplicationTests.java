b7991927c613f9d1c2b8502a996203457c73f44a
AryanJindal7 <aryanjindal103@gmail.com>
2024-11-17 16:39:25 +0530

    Adding microservices config files

---
+++ src/test/java/com/vrythion/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.vrythion.configserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ConfigserverApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
